1ef4c7076310f5010b608303c284b97125f34ef8
Ch. Kleideris <kleiderisch@gmail.com>
2025-05-21 23:57:00 +0300

    feat: add cf logo

---
+++ src/components/CodingFactoryLogo.tsx
@@ -0,0 +1,12 @@
+const CodingFactoryLogo = () => {
+    return (
+        <>
+            <img
+                className="my-4 h-16"
+                src="/logo.png"
+                alt="cf7-logo"/>
+        </>
+    )
+}
+
+export default CodingFactoryLogo;
